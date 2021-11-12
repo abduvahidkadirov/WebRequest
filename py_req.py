9877495ec7f9baeef64267280b6fb6d1c123f70b
abduvahidkadirov <abdu2003@mail.ru>
2021-11-12 11:03:56 +0500

    nextTest5

--- py_req.py
+++ py_req.py
@@ -1,6 +1,6 @@
 import requests as rq
 
-v_url = "https://10.36.0.73:5055/check?name=test&patronym=testovich&lastname=testov&username=tcell_2ad5kn21e88w36ag"
+
 r = rq.get(v_url , verify=False)
 print('Content-Type: text/json')
 print("")
