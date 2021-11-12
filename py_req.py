1715b9a6d29d0ada101d390ecfccd0cd6735b3ca
abduvahidkadirov <abdu2003@mail.ru>
2021-11-12 10:59:24 +0500

    nextTest3

--- py_req.py
+++ py_req.py
@@ -1,4 +1,9 @@
-import requests
+import requests as rq
 
-print("HellOOOOO")
+v_url = "https://10.36.0.73:5055/check?name=test&patronym=testovich&lastname=testov&username=tcell_2ad5kn21e88w36ag"
+r = rq.get(v_url , verify=False)
+print('Content-Type: text/plain')
+print("")
+
+print(r.text)
 
