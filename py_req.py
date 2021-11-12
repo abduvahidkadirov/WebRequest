1e32480da33251bb1ef27eccbac6b684f1301c96
abduvahidkadirov <abdu2003@mail.ru>
2021-11-12 16:52:25 +0500

    nextTest6

--- py_req.py
+++ py_req.py
@@ -1,9 +1,19 @@
 import requests as rq
+import json
+import req_ini as vals
+
+r = rq.get(vals.v_url , verify=False)
+print(r.status_code)
+print("++++++")
+
+json_string = json.dumps(r.text)
+print(json_string)
+
+json_data = json.loads(json_string)
+
+print("++++++")
+print(json_string['Answer'])
 
 
-r = rq.get(v_url , verify=False)
-print('Content-Type: text/json')
-print("")
 
-print(r.text)
-
+#print("Name: ", vals.get_name)
